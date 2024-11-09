882826aaaff77005d5c994b7ce3302b783dc3b6c
Vigneswaran Chinnasami <143638293+Vigneshxz@users.noreply.github.com>
2024-11-09 08:41:28 -0500

    node

---
+++ data.js
@@ -0,0 +1,61 @@
+// data.js
+const players = [
+    {
+        "_id": 1,
+        "name": "Thibaut Courtois",
+        "position": "Goalkeeper",
+        "nationality": "Belgium",
+        "img_name": "/images/Thibaut-Courtois.jpeg"
+    },
+    {
+        "_id": 2,
+        "name": "Dani Carvajal",
+        "position": "Defender",
+        "nationality": "Spain",
+        "img_name": "/images/danicarv.jpeg"
+    },
+    {
+        "_id": 3,
+        "name": "Antonio Rüdiger",
+        "position": "Defender",
+        "nationality": "Germany",
+        "img_name": "/images/rudiger.jpeg"
+    },
+    {
+        "_id": 4,
+        "name": "David Alaba",
+        "position": "Defender",
+        "nationality": "Austria",
+        "img_name": "/images/alaba.jpeg"
+    },
+    {
+        "_id": 5,
+        "name": "Eduardo Camavinga",
+        "position": "Midfielder",
+        "nationality": "France",
+        "img_name": "/images/cama.jpeg"
+    },
+    {
+        "_id": 6,
+        "name": "Luka Modrić",
+        "position": "Midfielder",
+        "nationality": "Croatia",
+        "img_name": "/images/modric.jpeg"
+    },
+    {
+        "_id": 7,
+        "name": "Vinícius Júnior",
+        "position": "Forward",
+        "nationality": "Brazil",
+        "img_name": "/images/vini.jpeg"
+    },
+    {
+        "_id": 8,
+        "name": "Diaz",
+        "position": "Forward",
+        "nationality": "Spain",
+        "img_name": "/images/diaz.jpeg"
+    }
+];
+
+module.exports = players;
---
+++ server.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const path = require('path');
+const players = require('./data');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Serve static files (images and CSS)
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(express.static('public'));
+
+// Endpoint to get all players
+app.get('/api/players', (req, res) => {
+  res.json(players);
+});
+
+// Serve index.html for the API documentation
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
