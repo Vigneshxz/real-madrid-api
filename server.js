88e3bf6bdf43b3b6a78f6763a9646bfd6a91fe02
Vigneswaran Chinnasami <143638293+Vigneshxz@users.noreply.github.com>
2024-11-09 11:08:37 -0500

    errors fixed

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-
 const express = require('express');
 const path = require('path');
+const cors = require('cors'); // Import the CORS package
 const players = require('./data');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Enable CORS for all routes
+app.use(cors());
 
 // Serve static files (images and CSS)
 app.use('/images', express.static(path.join(__dirname, 'images')));
@@ -15,6 +18,7 @@ app.get('/api/players', (req, res) => {
   res.json(players);
 });
 
+// Serve index.html for the root route
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
